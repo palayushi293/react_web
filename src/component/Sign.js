07f295c9fee28430d27b83e5e38084f254afd4ed
Ayushi Pal <palayushi293@gmail.com>
2024-08-01 11:15:37 +0530

    p

--- src/component/Sign.js
+++ src/component/Sign.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import video from './Smoky_Background.mp4';
 import "./Sig.css"
 export default function Sign() {
   return (
     <>
-      <div>
-        <div className="sign" style={{ height: 450, width: 500, marginLeft: 'auto', marginRight: 'auto', marginTop: 60, backgroundColor: "black", color: "white", padding: 20 }}>
+      <div >
+        <div classNmae="v">
+        
+       
+        <div className="sign" style={{ height: 450, width: 500, marginLeft: 'auto', marginRight: 'auto', marginTop: 60, color: "white", padding: 20 }}>
+          <video src={video} autoPlay loop muted>
           <h1 style={{ color: "white", textAlign: "center ", borderTop:100 }}>Signup</h1>
           <div style={{ marginTop: 100 }}></div>
           <input type="text" placeholder="Enter email" style={{ height: 40, width: 400, borderRadius: 15, textAlign: "center", marginBottom: 20 }} />
           <input type="password" placeholder="Enter password" style={{ height: 40, width: 400, borderRadius: 15, textAlign: "center", marginBottom: 20 }} />
           <p style={{ textAlign: "center" }}>Have you? <a href="#" style={{ color: "white" }}>Forgot Password</a></p>
+         </video>
         </div>
+       
+      </div>
+     
       </div>
     </>
   );
